3096285fc2a52cd1f9ef41f0c4fc6aaf159e3eb6
Viktoriia Khoroshun <vhoroshun.face@gmail.com>
2023-11-13 23:49:12 +0200

    Added decresing product in the cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,8 +1,9 @@
 class LineItemsController < ApplicationController
   include CurrentCart
 
-  before_action :set_cart, only: %i[create]
+  before_action :set_cart, only: %i[create decrease]
   before_action :set_line_item, only: %i[ show edit update destroy ]
+  before_action :set_product, only: %i[ create decrease ]
 
   # GET /line_items or /line_items.json
   def index
@@ -24,17 +25,30 @@ class LineItemsController < ApplicationController
 
   # POST /line_items or /line_items.json
   def create
-    product = Product.find(params[:product_id])
-    @line_item = @cart.add_product(product)
+    @line_item = @cart.add_product(@product)
     session[:counter] = 0
 
     respond_to do |format|
       if @line_item.save
-        format.turbo_stream { @current_item = @line_item }
+        format.turbo_stream { @current_item = @line_item; @notice = "Line item was successfully created." }
         format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def decrease
+    @line_item = @cart.decrease_product(@product)
+    respond_to do |format|
+      if @line_item && @line_item.save
+        format.turbo_stream { @current_item = @line_item; @notice = "Line item was successfully deleted." }
+        format.html { redirect_to store_index_url }
+        format.json { render :show, status: :ok, location: @line_item }
+      else
+        format.html { redirect_to store_index_url }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
@@ -74,5 +88,9 @@ class LineItemsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def line_item_params
       params.require(:line_item).permit(:product_id)
+    end
+
+    def set_product
+      @product = Product.find(params[:product_id])
     end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,6 +11,16 @@ class Cart < ApplicationRecord
     current_item
   end
 
+  def decrease_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+
+    if current_item
+      current_item.quantity > 1 ? current_item.quantity -=1 : current_item.destroy
+    end
+
+    current_item
+  end
+
   def total_price
     line_items.sum { |item| item.total_price }
   end
