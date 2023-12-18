5a9c12c4f395d726d851b7a1c219b0fc06152e58
Viktoriia Khoroshun <vhoroshun.face@gmail.com>
2023-12-18 23:29:23 +0200

    added suport requests

--- app/mailboxes/support_mailbox.rb
+++ app/mailboxes/support_mailbox.rb
@@ -1,9 +1,13 @@
 class SupportMailbox < ApplicationMailbox
   def process
-    puts "START SupportMailbox#process:"
-    puts "From: #{mail.from}"
-    puts "Subject: #{mail.subject}"
-    puts "Body: #{mail.body}"
-    puts "END SupportMailbox#process:"
+    p mail.from[0].to_s
+    recent_order = Order.where(email: mail.from[0].to_s).order('created_at desc').first
+    p recent_order
+    SupportRequest.create!(
+      email: mail.from[0].to_s,
+      subject: mail.subject,
+      body: mail.body.to_s,
+      order: recent_order
+    )
   end
 end
